400a5db7068af6fcc63af6348c8d257a372998ff
soludes <162672707+soludes@users.noreply.github.com>
2024-03-08 19:25:08 +1300

    Create odd_numbers.py

---
+++ odd_numbers.py
@@ -0,0 +1,11 @@
+################################################################################
+#
+# Program: Print Odd Numbers In A List
+#
+# Description: A program to print the odd numbers in a list using Python.
+#
+# YouTube Lesson: https://www.youtube.com/watch?v=B56J6p_v_zY
+#
+# Author: Kevin Browne @ https://portfoliocourses.com
+#
+################################################################################
